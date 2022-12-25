72dabb3fb4d30075133dd6f0199a420fbe3fbbc9
Santiago Carreira <stgocarreira@gmail.com>
2022-12-25 20:22:23 -0300

    tercer commit correccion

--- src/index.js
+++ src/index.js
@@ -9,10 +9,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
       <div class="redes">
-        <a href="https://www.facebook.com/santiagoroberto.carreira"><i class="fab fa-facebook"></i></a>
-        <a href="https://www.linkedin.com/in/santiago-carreira-99a12522a/"><i class="fab fa-linkedin"></i></a>
-        <a href="https://twitter.com/SantiagoCarrei6"><i class="fab fa-twitter"></i></a>
-        <a href="https://github.com/Santiago65"><i class="fab fa-github"></i></a>
+        <a href="https://www.facebook.com/santiagoroberto.carreira"target="_blank" rel="noreferrer"><i class="fab fa-facebook"></i></a>
+        <a href="https://www.linkedin.com/in/santiago-carreira-99a12522a/"target="_blank" rel="noreferrer"><i class="fab fa-linkedin"></i></a>
+        <a href="https://twitter.com/SantiagoCarrei6"target="_blank" rel="noreferrer"><i class="fab fa-twitter"></i></a>
+        <a href="https://github.com/Santiago65"target="_blank" rel="noreferrer"><i class="fab fa-github"></i></a>
         
     </div>
     <h1> Calculadora Científica</h1>
